bfb699ba1297ac4029c07373272c0be0c8b9f8ef
Suraj Singh <165886618+singhsrj@users.noreply.github.com>
2024-09-24 01:18:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-st.title("Salary Prediction")
+import numpy as np
+import pandas as pd
+
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X= dataset.iloc[:,:-1].values
+y=dataset.iloc[:,-1].values
+
+"""Spliting dataset into training set and testing set"""
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state = 0)
+
+"""Feature Scale the Estimated salary"""
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.tree import DecisionTreeClassifier
+classifier =DecisionTreeClassifier(criterion ='log_loss', random_state=0)
+
+classifier.fit(X_train,y_train)
+
+
+"""Prediction for whole y_test"""
+
+#here , we concatenate the prediction with
+#our testing set, to check whether the prediction is right or not
+
+st.title("whether customer will buy or not")
 
 # Sidebar for user inputs
 st.sidebar.header("Input Features")
@@ -27,15 +58,13 @@ st.write(input_df)
 
 # Make prediction with the loaded model
 prediction = classifier.predict(input_df)
-prediction_proba = classifier.predict_proba(input_df)
+# prediction_proba = classifier.predict_proba(input_df)
 
-# Display the class names and corresponding prediction
-st.subheader('Class Labels and their Index Number')
-st.write(['Setosa', 'Versicolour', 'Virginica'])
+# # Display the class names and corresponding prediction
+# st.subheader('Class Labels and their Index Number')
+# st.write(['Setosa', 'Versicolour', 'Virginica'])
 
 # Show the predicted class and the prediction probability
 st.subheader('Prediction')
-st.write(['Setosa', 'Versicolour', 'Virginica'][prediction[0]])
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+if(prediction==1) st.write("Yes")
+else st.write("NO')
